4cd78e2f0e84527090ce78f0443e8e78d16c6da1
botagari <48246327+botagari@users.noreply.github.com>
2019-05-09 22:46:02 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "#";
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+if(!message.channel.guild) return
+   let embed = new Discord.RichEmbed()
+   let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.startsWith('$bc')) {
+       message.guild.members.forEach(member => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+           member.send(` ${args}`);
+
+        });
+    }
+    
+
+});
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
